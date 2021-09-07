a312330371e87008cdf8b60218a7b8aa4f2aaca9
Brandon White <bjwhite@fnal.gov>
2021-09-07 16:48:22 +0000

    Use podman if on Redhat.

--- start_container.sh
+++ start_container.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+release=`python -mplatform`
+if [[ ${release} == *redhat* ]]; then
+    container_manager='podman'
+fi
 
 # Start up the Rubin Data Management container, mounting dev/ into it.
-docker run \
+echo "manager: ${container_manager}"
+${container_manager} run \
     -v ${PWD}/dev:/home/lsst/dev \
     -d \
     -it \
